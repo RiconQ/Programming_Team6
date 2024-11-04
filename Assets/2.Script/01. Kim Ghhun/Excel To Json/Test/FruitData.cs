75f2c50ce5e06aa7b204e7dc1e3775fe62d16132
김지훈 <ricon4246@gmail.com>
2024-11-04 11:54:25 +0900

    [Update] Parser Fix

--- Assets/2.Script/01. Kim Ghhun/Excel To Json/Test/FruitData.cs
+++ Assets/2.Script/01. Kim Ghhun/Excel To Json/Test/FruitData.cs
@@ -22,13 +22,12 @@ public class FruitData : ScriptableObject
        
         for (int i = 0; i < fruits.Length; i++)
         {
-            int idx = 0;
             FruitAttribute attribute = new FruitAttribute();
-            attribute.index = (int)(double)jsonData[i][idx++];
-            attribute.name = jsonData[i][idx++].ToString();
-            attribute.mass = (float)(double)jsonData[i][idx++];
-            attribute.scaleX = (float)(double)jsonData[i][idx++];
-            attribute.scaleY = (float)(double)jsonData[i][idx++];
+            attribute.index = (int)(double)jsonData[i]["index"];
+            attribute.name = jsonData[i]["name"].ToString();
+            attribute.mass = (float)(double)jsonData[i]["mass"];
+            attribute.scaleX = (float)(double)jsonData[i]["scaleX"];
+            attribute.scaleY = (float)(double)jsonData[i]["scaleY"];
 
             fruits[i].SetAttribute(attribute);
         }
